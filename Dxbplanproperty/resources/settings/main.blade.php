ba19fb1d68c67b6d28874728d0e06cfe3504a04f
kestherboy <kester@anyvan.com>
2019-04-10 17:23:09 +0100

    initial commit 2

---
+++ Dxbplanproperty/resources/settings/main.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @yield('head')
+    <title> @yield('title')</title>
+</head>
+<body>
+<h1>Hello</h1>
+@yield('main')
+@yield('footer')
+@yield('script')
+</body>
+</html>
---
+++ Dxbplanproperty/resources/views/landing-page.blade.php
@@ -0,0 +1,1 @@
+@extends('setting.main')
