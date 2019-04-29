0f4484f695fd3cabbeed6e69535faec5c5b126ee
Pekun Apara <pekun.apara@ericsson.com>
2019-04-29 23:24:14 +0100

    pk

--- Dxbplanproperty/resources/views/contactUs.blade.php
+++ Dxbplanproperty/resources/views/contactUs.blade.php
@@ -130,7 +130,7 @@
 <!-- Google map start -->
 <div class="section">
     <div class="map">
-        <div id="contactMap" class="contact-map"></div>
+        <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=2%20ryeriver%20leixlip%20kildare&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{position:relative;text-align:right;height:100%;width:100%;}.gmap_canvas {overflow:hidden;background:none!important;height:100%;width:100%;}</style></div>
     </div>
 </div>
 <!-- Google map end -->
